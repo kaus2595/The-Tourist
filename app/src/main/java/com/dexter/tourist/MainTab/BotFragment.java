e4d4df7b267d5dbc8dcac3aabbb4fbec42fcdf91
kaus2595 <kausr2595@gmail.com>
2018-03-21 04:25:56 +0530

    Chat bot

--- app/src/main/java/com/dexter/tourist/MainTab/BotFragment.java
+++ app/src/main/java/com/dexter/tourist/MainTab/BotFragment.java
@@ -3,8 +3,10 @@ package com.dexter.tourist.MainTab;
 import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
 import android.text.TextUtils;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -15,6 +17,7 @@ import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.ProgressBar;
@@ -31,12 +34,16 @@ import java.util.List;
 import java.util.Map;
 
 import ai.api.AIListener;
+import ai.api.AIServiceException;
 import ai.api.RequestExtras;
 import ai.api.android.AIConfiguration;
+import ai.api.android.AIDataService;
 import ai.api.android.AIService;
 import ai.api.android.GsonFactory;
 import ai.api.model.AIContext;
 import ai.api.model.AIError;
+import ai.api.model.AIEvent;
+import ai.api.model.AIRequest;
 import ai.api.model.AIResponse;
 import ai.api.model.Metadata;
 import ai.api.model.Result;
@@ -44,19 +51,21 @@ import ai.api.model.Status;
 
 import static com.google.android.gms.internal.zzahn.runOnUiThread;
 
-public class BotFragment extends Fragment implements AIListener, AdapterView.OnItemSelectedListener {
+public class BotFragment extends Fragment implements AdapterView.OnItemSelectedListener, View.OnClickListener {
 
     public static final String TAG = BotFragment.class.getName();
 
-    private AIService aiService;
-    private ProgressBar progressBar;
-    private ImageView recIndicator;
-    private TextView t;
+    private Gson gson = GsonFactory.getGson();
+
+    private TextView resultTextView;
     private EditText contextEditText;
+    private EditText queryEditText;
+    private CheckBox eventCheckBox;
+    private Spinner eventSpinner;
+    private AIDataService aiDataService;
     Button listen;
     View bot_view;
 
-    private Gson gson = GsonFactory.getGson();
 
     public BotFragment() {
         // Required empty public constructor
@@ -67,116 +76,113 @@ public class BotFragment extends Fragment implements AIListener, AdapterView.OnI
         super.onCreate(savedInstanceState);
 
     }
-
-    @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         bot_view = inflater.inflate(R.layout.fragment_bot, container, false);
-        t = (TextView)bot_view.findViewById(R.id.resultTextView);
-        progressBar = (ProgressBar)bot_view.findViewById(R.id.progressBar);
         contextEditText = (EditText)bot_view.findViewById(R.id.contextEditText);
-        listen = (Button)bot_view.findViewById(R.id.buttonListen);
+
+        resultTextView = (TextView)bot_view.findViewById(R.id.resultTextView);
+        contextEditText = (EditText)bot_view.findViewById(R.id.contextEditText);
+        queryEditText = (EditText)bot_view.findViewById(R.id.textQuery);
+
+        bot_view.findViewById(R.id.buttonSend).setOnClickListener(this);
+        bot_view.findViewById(R.id.buttonClear).setOnClickListener(this);
+
+        eventSpinner = (Spinner)bot_view.findViewById(R.id.selectEventSpinner);
+        final ArrayAdapter<String> eventAdapter = new ArrayAdapter<String>( getContext(),android.R.layout.simple_spinner_dropdown_item, Config.events);
+        eventSpinner.setAdapter(eventAdapter);
+
+//        Spinner spinner = (Spinner)bot_view.findViewById(R.id.selectLanguageSpinner);
+//        final ArrayAdapter<LanguageConfig> languagesAdapter = new ArrayAdapter<String>(getContext() ,android.R.layout.simple_spinner_dropdown_item, Config.languages);
+//        spinner.setAdapter(languagesAdapter);
+//        spinner.setOnItemSelectedListener(this);
+       return bot_view;
+    }
 
 //        Spinner spinner = (Spinner)bot_view.findViewById(R.id.selectLanguageSpinner);
 //        final ArrayAdapter<LanguageConfig> languagesAdapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, Config.languages);
 //        spinner.setAdapter(languagesAdapter);
 //        spinner.setOnItemSelectedListener(this);
 
-        listen.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startRecognition(v);
-
-            }
-        });
-
-        final AIConfiguration config = new AIConfiguration("62e6f6b827d04e51b19660a051f95e4a",
-                AIConfiguration.SupportedLanguages.English,
-                AIConfiguration.RecognitionEngine.System);
-        AIService aiService = AIService.getService(getContext(),config);
-        aiService.setListener(this);
-
-        return bot_view;
-    }
-//    @Override
-//    public void onResult(AIResponse result) {
-//        Log.d("Tweek",result.toString());
-//        Result result1 = result.getResult();
-//        t.setText(result1.getResolvedQuery() + "action : " + result1.getAction());
-//    }
-
     private void initService(final LanguageConfig selectedLanguage) {
         final AIConfiguration.SupportedLanguages lang = AIConfiguration.SupportedLanguages.fromLanguageTag(selectedLanguage.getLanguageCode());
         final AIConfiguration config = new AIConfiguration(selectedLanguage.getAccessToken(),
                 lang,
                 AIConfiguration.RecognitionEngine.System);
+        aiDataService = new AIDataService(getContext(),config);
+    }
+private void clearEditText() {
+    queryEditText.setText("");
+}
 
-        if (aiService != null) {
-            aiService.pause();
+    /*
+    * AIRequest should have query OR event
+    */
+    private void sendRequest() {
+
+        final String queryString = !eventSpinner.isEnabled() ? String.valueOf(queryEditText.getText()) : null;
+        final String eventString = eventSpinner.isEnabled() ? String.valueOf(String.valueOf(eventSpinner.getSelectedItem())) : null;
+        final String contextString = String.valueOf(contextEditText.getText());
+
+        if (TextUtils.isEmpty(queryString) && TextUtils.isEmpty(eventString)) {
+            onError(new AIError(getString(R.string.non_empty_query)));
+            return;
         }
 
-        aiService = AIService.getService(getContext(), config);
-        aiService.setListener(this);
+        final AsyncTask<String, Void, AIResponse> task = new AsyncTask<String, Void, AIResponse>() {
+
+            private AIError aiError;
+
+            @Override
+            protected AIResponse doInBackground(final String... params) {
+                final AIRequest request = new AIRequest();
+                String query = params[0];
+                String event = params[1];
+
+                if (!TextUtils.isEmpty(query))
+                    request.setQuery(query);
+                if (!TextUtils.isEmpty(event))
+                    request.setEvent(new AIEvent(event));
+                final String contextString = params[2];
+                RequestExtras requestExtras = null;
+                if (!TextUtils.isEmpty(contextString)) {
+                    final List<AIContext> contexts = Collections.singletonList(new AIContext(contextString));
+                    requestExtras = new RequestExtras(contexts, null);
+                }
+                try {
+                    return aiDataService.request(request, requestExtras);
+                } catch (AIServiceException e1) {
+                    e1.printStackTrace();
+                    return null;
+                }
+            }
+
+//            @Override
+//            protected void onPostExecute(final AIResponse response) {
+//                if (response != null) {
+//                    onResult(response);
+//                } else {
+//                    onError(aiError);
+//                }
+//            }
+        };
+
+        task.execute(queryString, eventString, contextString);
     }
 
-//    @Override
-//    public boolean onCreateOptionsMenu(final Menu menu) {
-//        getMenuInflater().inflate(R.menu.menu_aiservice_sample, menu);
-//        return true;
-//    }
-
-//    @Override
-//    public boolean onOptionsItemSelected(final MenuItem item) {
-//        final int id = item.getItemId();
-//
-//        //noinspection SimplifiableIfStatement
-//        if (id == R.id.action_settings) {
-//            startActivity(AISettingsActivity.class);
-//            return true;
-//        }
-//        return super.onOptionsItemSelected(item);
-//    }
-
-
-
-    public void stopRecognition(final View view) {
-        aiService.stopListening();
+    public void checkBoxClicked() {
+        eventSpinner.setEnabled(eventCheckBox.isChecked());
+        queryEditText.setVisibility(!eventCheckBox.isChecked() ? View.VISIBLE : View.GONE);
     }
 
-    public void cancelRecognition(final View view) {
-        aiService.cancel();
-    }
-
-    @Override
-    public void onPause() {
-        super.onPause();
-
-        // use this method to disconnect from speech recognition service
-        // Not destroying the SpeechRecognition object in onPause method would block other apps from using SpeechRecognition service
-        if (aiService != null) {
-            aiService.pause();
-        }
-    }
-
-    @Override
-    public void onResume() {
-        super.onResume();
-
-        // use this method to reinit connection to recognition service
-        if (aiService != null) {
-            aiService.resume();
-        }
-    }
-
-    @Override
-    public void onResult(final AIResponse response) {
+    private void onResult(final AIResponse response) {
         runOnUiThread(new Runnable() {
             @Override
             public void run() {
                 Log.d(TAG, "onResult");
 
-                t.setText(gson.toJson(response));
+                resultTextView.setText(gson.toJson(response));
 
                 Log.i(TAG, "Received success response");
 
@@ -210,75 +216,24 @@ public class BotFragment extends Fragment implements AIListener, AdapterView.OnI
             }
 
         });
-}
+    }
 
-    @Override
-    public void onError(final AIError error) {
+    private void onError(final AIError error) {
         runOnUiThread(new Runnable() {
             @Override
             public void run() {
-                t.setText(error.toString());
+                resultTextView.setText(error.toString());
             }
         });
     }
 
-    public void startRecognition(final View view) {
-        final String contextString = String.valueOf(contextEditText.getText());
-        if (TextUtils.isEmpty(contextString)) {
-            aiService.startListening();
-        } else {
-            final List<AIContext> contexts = Collections.singletonList(new AIContext(contextString));
-            final RequestExtras requestExtras = new RequestExtras(contexts, null);
-            aiService.startListening(requestExtras);
-        }
 
+    private void startActivity(Class<?> cls) {
+        final Intent intent = new Intent(getContext(),cls);
+        startActivity(intent);
     }
 
-    @Override
-    public void onAudioLevel(final float level) {
-        runOnUiThread(new Runnable() {
-            @Override
-            public void run() {
-                float positiveLevel = Math.abs(level);
 
-                if (positiveLevel > 100) {
-                    positiveLevel = 100;
-                }
-                progressBar.setProgress((int) positiveLevel);
-            }
-        });
-    }
-
-    @Override
-    public void onListeningStarted() {
-        runOnUiThread(new Runnable() {
-            @Override
-            public void run() {
-                recIndicator.setVisibility(View.VISIBLE);
-            }
-        });
-    }
-
-    @Override
-    public void onListeningCanceled() {
-        runOnUiThread(new Runnable() {
-            @Override
-            public void run() {
-                recIndicator.setVisibility(View.INVISIBLE);
-                t.setText("");
-            }
-        });
-    }
-
-    @Override
-    public void onListeningFinished() {
-        runOnUiThread(new Runnable() {
-            @Override
-            public void run() {
-                recIndicator.setVisibility(View.INVISIBLE);
-            }
-        });
-    }
 
     @Override
     public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
@@ -288,12 +243,21 @@ public class BotFragment extends Fragment implements AIListener, AdapterView.OnI
 
     @Override
     public void onNothingSelected(AdapterView<?> parent) {
-
     }
 
-//    private void startActivity(Class<?> cls) {
-//        final Intent intent = new Intent(this, cls);
-//        startActivity(intent);
-//    }
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.buttonClear:
+                clearEditText();
+                break;
+            case R.id.buttonSend:
+                sendRequest();
+                break;
+            case R.id.eventsCheckBox:
+                checkBoxClicked();
+                break;
+        }
+    }
 }
 
